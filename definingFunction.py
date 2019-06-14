6739876ca7a5b163e0428a173aadae125a7b4af0
Shrikant Badiger <shrikant200@gmail.com>
2019-06-14 23:13:41 +0530

    changes to print the fib numbers

--- definingFunction.py
+++ definingFunction.py
@@ -1,14 +1,13 @@
 #Program to illustrate the defining the function
 
 
-def fib(n):
+def FIB(n):
     print("Entry of Fib")
     """ Print the Fib series up to n """
     a, b = 0, 1
     while a < n:
-        print(" I am here")
         print(a, end=' ')
-        a, b = b, a+bx
+        a, b = b, a+b
     print()
 
-fib(10)
+FIB(10)
